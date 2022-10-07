a1f56830ea79c97a47bc95cc6406d14ea60a4fd6
Prince <PrincePR8920@gmail.com>
2022-10-07 14:38:09 +0530

    some minor changes

--- filter.js
+++ filter.js
@@ -5,15 +5,15 @@ module.exports = class Filter{
    if (Array.isArray(info)) {
      info.forEach((doc) => {
        exclude.forEach((elem) => {
-         if (doc[elem]) {
-           delete doc[elem];
+         if (doc?.[elem]) {
+           delete doc?.[elem];
          }
        });
      });
    } else {
      exclude.forEach((elem) => {
-       if (filterdInfo[elem] || !filterdInfo[elem]) {
-         delete filterdInfo[elem];
+       if (filterdInfo?.[elem] || !filterdInfo?.[elem]) {
+         delete filterdInfo?.[elem];
        }
      });
    }
